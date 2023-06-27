4a6cc6de02f18246793c15cbb9de623e5a4c7f11
Raz <raz_secret@outlook.com>
2023-06-28 02:19:54 +0300

    Added more screens

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import OpeningScreen from './src/screens/OpeningScreen';
+import Login from './src/screens/Login';
+import Register from './src/screens/Register';
+import Home from './src/screens/Home';
+import ForgotPassword from './src/screens/ForgotPassword';
+import UsersContextProvider from './src/context/UsersContext';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <UsersContextProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} options={{headerTransparent: true, headerTitle: '',headerLeft: null }}/>
+          <Stack.Screen name="Home" component={Home} options={{headerTransparent: true, headerTitle: '' }}/>
+          <Stack.Screen name="Register" component={Register} options={{headerTransparent: true, headerTitle: '', headerLeft: null}}/>
+          <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{headerTransparent: true, headerTitle: '' ,headerLeft: null}}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </UsersContextProvider>
   );
 }
 
