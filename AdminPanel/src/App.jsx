7879d7dd9bccd8fb396f61a1dab679f1a7d4e962
Raz <raz_secret@outlook.com>
2023-11-18 00:07:20 +0200

    commit

--- AdminPanel/src/App.jsx
+++ AdminPanel/src/App.jsx
@@ -6,7 +6,6 @@ import Admin from './components/Admin';
 import UsersList from './components/UsersList';
 import Hotels from './components/Hotels';
 import Flights from './components/Flights';
-import BookingHotels from './components/Bookinghotels'
 import BookingFlights from './components/BookingFlights';
 import Support from './components/Support';
 import AdminContextProvidewr from './contexts/AdminContext';
@@ -24,7 +23,6 @@ function App() {
           <Route path="/admin/flights" element={<PrivateRoute><Flights /></PrivateRoute>} />
           <Route path="/admin/users" element={<PrivateRoute><UsersList /></PrivateRoute>} />
           <Route path="/admin/support" element={<PrivateRoute><Support /></PrivateRoute>} />
-          <Route path="/admin/bookingHotels" element={<PrivateRoute><BookingHotels /></PrivateRoute>} />
           <Route path="/admin/bookingflights" element={<PrivateRoute><BookingFlights /></PrivateRoute>} />
         </Routes>
       </Router>
